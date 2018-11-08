e7a084967060d39f13db2037f90edb01886f62b0
BaptisteAngot <bat76640@gmail.com>
2018-11-08 12:17:22 +0100

    resetstyle

--- inc/header.php
+++ inc/header.php
@@ -2,7 +2,7 @@
 <html lang="fr" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="asset/index.css">
+    <link rel="stylesheet" href="asset/css/style.css">
     <title></title>
   </head>
   <body>
