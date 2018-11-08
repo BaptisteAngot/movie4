e2ce4fc12a8749770ae4e3f19c4a10b9afe67aea
BaptisteAngot <bat76640@gmail.com>
2018-11-08 14:34:34 +0100

    detail

--- detail.php
+++ detail.php
@@ -37,13 +37,17 @@ else {
     <h1><?php echo $movie['title']; ?></h1>
     <?php imageMovie($movie); ?>
     <p class="year">Date de sortie: <?php echo $movie['year'];?></p>
-    <p class="time">Durée: <?php echo convertToHoursMins($movie['runtime'],'%02d heures %02d minutes'); ?></p>
+    <p class="time">Durée: <?php echo convertToHoursMins($movie['runtime'],'%02d heure(s) %02d minute(s)'); ?></p>
     <p class="directors">De: <?php echo $movie['directors']; ?></p>
     <p class="cast">Avec: <?php echo $movie['cast']; ?> </p>
     <p class="genre">Genre: <?php echo $movie['genres']; ?> </p>
     <p class="mpaa">Classification age: <?php echo $movie['mpaa'];?></p>
     <p class="popularity">Popularité: <?php echo $movie['popularity'];?></p>
-    <p class="rating">Rating: <?php echo $movie['rating'];?></p>
+<div class="star-ratings-sprite"><span style="width: <?php echo $movie['rating'];?>%" class="star-ratings-sprite-rating"></span></div>
+    <?php if (isLogged()) { ?>
+      <a href="addlist.php">Ajouter à la liste</a>
+    <?php } ?>
 </div>
+
 
 <?php include('inc/footer.php'); ?>
