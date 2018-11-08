bf289b35d6fe5360026d42c5d4981a30baec0722
Druxys <drudrux@gmail.com>
2018-11-08 11:46:07 +0100

    navbar

---
+++ inc/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="assets/index.css">
+    <title>Register</title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li><a href="index.php">Acceuil</a></li>
+          <?php if (isLogged()) { ?>
+            <li><a href="deconnexion.php">Déconnexion</a></li>
+          <?php } else { ?>
+            <?php if (isAdmin()) { ?>
+              <li><a href="dashboard.php">Admin</a></li>
+            <?php } ?>
+            <li><a href="inscription.php">Inscription</a></li>
+            <li><a href="connexion.php">Connexion</a></li>
+          <?php } ?>
+        </ul>
+      </nav>
+    </header>
