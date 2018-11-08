4dbb03d942f93e1c96849e4cdb90c8e05671b464
BaptisteAngot <bat76640@gmail.com>
2018-11-08 15:15:11 +0100

    debug

--- detail.php
+++ detail.php
@@ -34,16 +34,16 @@ else {
 <?php include('inc/header.php'); ?>
 
 <div class="film">
-    <h1><?php echo $movie['title']; ?></h1>
-    <?php imageMovie($movie); ?>
-    <p class="year">Date de sortie: <?php echo $movie['year'];?></p>
-    <p class="time">Durée: <?php echo convertToHoursMins($movie['runtime'],'%02d heure(s) %02d minute(s)'); ?></p>
-    <p class="directors">De: <?php echo $movie['directors']; ?></p>
-    <p class="cast">Avec: <?php echo $movie['cast']; ?> </p>
-    <p class="genre">Genre: <?php echo $movie['genres']; ?> </p>
-    <p class="mpaa">Classification age: <?php echo $movie['mpaa'];?></p>
-    <p class="popularity">Popularité: <?php echo $movie['popularity'];?></p>
-<div class="star-ratings-sprite"><span style="width: <?php echo $movie['rating'];?>%" class="star-ratings-sprite-rating"></span></div>
+      <h1><?php echo $movie['title']; ?></h1>
+      <?php imageMovie($movie); ?>
+      <p class="year">Date de sortie: <?php echo $movie['year'];?></p>
+      <p class="time">Durée: <?php echo convertToHoursMins($movie['runtime'],'%02d heure(s) %02d minute(s)'); ?></p>
+      <p class="directors">De: <?php echo $movie['directors']; ?></p>
+      <p class="cast">Avec: <?php echo $movie['cast']; ?> </p>
+      <p class="genre">Genre: <?php echo $movie['genres']; ?> </p>
+      <p class="mpaa">Classification age: <?php echo $movie['mpaa'];?></p>
+      <p class="popularity">Popularité: <?php echo $movie['popularity'];?></p>
+  <div class="star-ratings-sprite"><span style="width: <?php echo $movie['rating'];?>%" class="star-ratings-sprite-rating"></span></div>
 </div>
 
 
