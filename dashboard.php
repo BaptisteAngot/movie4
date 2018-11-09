42e27a7f5f5b4d26a776daef4dce6c4400a0d955
BaptisteAngot <bat76640@gmail.com>
2018-11-09 15:47:21 +0100

    Analytic Finish

--- dashboard.php
+++ dashboard.php
@@ -43,6 +43,33 @@ $query = $pdo -> prepare($sql);
 $query -> execute();
 $lastmovie=$query->fetch();
 
+//Affichage d'un genre
+$sql ="SELECT COUNT(genres) FROM movies_full WHERE genres LIKE '%drama%'";
+$query = $pdo -> prepare($sql);
+// $query -> bindValue(':genre',$genre,PDO::PARAM_INT);
+$query -> execute();
+$genred=$query->fetch();
+
+$sql ="SELECT COUNT(genres) FROM movies_full WHERE genres LIKE '%fantasy%'";
+$query = $pdo -> prepare($sql);
+// $query -> bindValue(':genre',$genre,PDO::PARAM_INT);
+$query -> execute();
+$genreF=$query->fetch();
+
+
+/////////////// 30 films ajouté
+$sql="SELECT m.title, COUNT(lm.movie_id) FROM movies_full AS m LEFT JOIN liste_movie AS lm ON m.id=lm.movie_id GROUP BY lm.movie_id ORDER BY COUNT(lm.movie_id) DESC LIMIT 0,30";
+$query = $pdo -> prepare($sql);
+$query -> execute();
+$best=$query->fetchAll();
+
+//Dernier avis
+$sql= "SELECT created_at FROM liste_movie ORDER BY created_at ASC";
+$query = $pdo -> prepare($sql);
+$query -> execute();
+$lastreview=$query->fetch();
+
+
 include ('inc/header_back.php');
  ?>
 <div class="content">
@@ -55,7 +82,7 @@ include ('inc/header_back.php');
           </div>
           <div class="card-body">
             <h4 class="card-title ">Nombre d'utilisateurs en temps réel</h4>
-            <p class="card-category"><span class="text-success"><i class="material-icons text-warning">account_circle</i></span><?php echo $nbusers; ?> Utilisateurs inscrit.</p>
+            <p class="card-category"><span class="text-success"><i class="material-icons text-warning">account_circle</i></span><?php echo $nbusers; ?> compte(s) inscrit(s).</p>
             <p class="card-category"><span class="text-success"><i class="material-icons text-danger">gavel</i></span><?php echo $nbadmin; ?> Admin.</p>
             <p class="card-category"><span class="text-success"><i class="material-icons text-sucess">face</i></span><?php echo $nbuser; ?> Utilisateur(s).</p>
           </div>
@@ -74,6 +101,8 @@ include ('inc/header_back.php');
         <div class="card-body">
           <h4 class="card-title ">Nombre de films en temps réel</h4>
           <p class="card-category"><span class="text-success"><i class="material-icons text-warning">movie_filter</i></span><?php echo $nbfilmsbdd['COUNT(id)']; ?> films en base de données.</p>
+          <p class="card-category"><span class="text-success"><i class="material-icons text-sucess">movie_filter</i></span><?php echo $genred['COUNT(genres)']; ?> films Drama en base de données.</p>
+          <p class="card-category"><span class="text-success"><i class="material-icons text-info">movie_filter</i></span><?php echo $genreF['COUNT(genres)']; ?> films Fantastique en base de données.</p>
         </div>
         <div class="card-footer">
           <div class="stats">
@@ -89,11 +118,14 @@ include ('inc/header_back.php');
       </div>
       <div class="card-body">
         <h4 class="card-title ">30 film le plus ajoutés à la liste des films à voir</h4>
-        <p class="card-category"><span class="text-success"><i class="material-icons text-warning">movie_filter</i></span><?php echo $nbfilmsbdd['COUNT(id)']; ?> films en base de données.</p>
+        <?php foreach ($best as $bfilm) {
+          echo '- ' . $bfilm['title'];
+          echo '</br>';
+        } ?>
       </div>
       <div class="card-footer">
         <div class="stats">
-          <p class="card-category"><span class="text-success"><i class="material-icons text-warning">movie_filter</i></span><?php echo $nbfilmsbdd['COUNT(id)']; ?> films en base de données.</p>
+          <p class="card-category"><span class="text-success"><i class="material-icons text-warning">access_time</i></span> Dernier film ajouté à la liste d'un utilisateur: <?php echo $lastreview['created_at']; ?></p>
         </div>
       </div>
     </div>
