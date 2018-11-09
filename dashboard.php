69664bcb7739715ec89554994f5baf4b130c2f14
BaptisteAngot <bat76640@gmail.com>
2018-11-09 09:31:39 +0100

    dashboardsecure

--- dashboard.php
+++ dashboard.php
@@ -1,8 +1,16 @@
 <?php
 $pagename="dashboard";
+include 'inc/pdo.php';
+include 'inc/function.php';
+// pre($_SESSION);
+if(isAdmin()){
 include 'inc/header_back.php';
  ?>
 
 <?php
 include 'inc/footer_back.php';
+}
+else {
+  die('403');
+}
  ?>
--- inc/header_back.php
+++ inc/header_back.php
@@ -1,7 +1,18 @@
 <?php
-  include('inc/pdo.php');
-  include('inc/function.php');
-
+  function afficherelement($element,$pagename){
+    echo '<li ';
+    if($element[0] == $pagename ){
+      echo 'class= "nav-item active">';
+    }
+    else{
+      echo 'class= "nav-item">';
+    }
+    echo '<a class= "nav-link" href='.$element[1].'>';
+    echo '<i class="material-icons">' .$element[2]. '</i>';
+    echo '<p>' . $element[3] . '</p>';
+    echo '</a>';
+    echo '</li>';
+  }
  ?>
 
 <!doctype html>
